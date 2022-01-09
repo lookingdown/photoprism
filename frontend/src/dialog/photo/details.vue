4ceb0e94e01bec9d9dbfed78d6163fc1968464a3
lookingdown <3320447+lookingdown@users.noreply.github.com>
2022-01-09 13:24:47 -0300

    Update details.vue

--- frontend/src/dialog/photo/details.vue
+++ frontend/src/dialog/photo/details.vue
@@ -2,7 +2,7 @@
   <div class="p-tab p-tab-photo-details">
     <v-container fluid>
       <v-form ref="form" lazy-validation
-              dense class="p-form-photo-details-meta" accept-charset="UTF-8"
+              dense class="p-form-photo-details-meta p-photo a-photo" accept-charset="UTF-8"
               @submit.prevent="save">
         <v-layout row wrap align-top fill-height>
           <v-flex
